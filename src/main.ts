cfdc1f9a3546d120ac7d673b333c02b8d5c0888b
Maicon Godinho <maicongodinho1@gmail.com>
2024-06-25 22:36:26 -0300

    chore: add @nestjs/config dependency and configure it in app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  const logger = new Logger('NestApplication');
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
